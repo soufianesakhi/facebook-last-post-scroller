85887e80021df41b39048a13d6ab8b39efb4177a
Soufiane Sakhi <soufiane.sakhi.js@gmail.com>
2016-03-17 01:03:08 +0100

    Fix 1.0.3: Execute script only on the homepage in most recent mode

--- script/Facebook_last_post_scroller.user.js
+++ script/Facebook_last_post_scroller.user.js
@@ -2,11 +2,17 @@
 // @name        Facebook last post scroller
 // @namespace   https://github.com/soufianesakhi/facebook-last-post-scroller
 // @author      Soufiane Sakhi
-// @license		MIT licensed, Copyright (c) 2016 Soufiane Sakhi (https://opensource.org/licenses/MIT)
+// @license     MIT licensed, Copyright (c) 2016 Soufiane Sakhi (https://opensource.org/licenses/MIT)
+// @homepage    https://github.com/soufianesakhi/facebook-last-post-scroller
+// @supportURL  https://github.com/soufianesakhi/facebook-last-post-scroller/issues
+// @updateURL   https://github.com/soufianesakhi/facebook-last-post-scroller/raw/master/script/Facebook_last_post_scroller.user.js
+// @downloadURL https://github.com/soufianesakhi/facebook-last-post-scroller/raw/master/script/Facebook_last_post_scroller.user.js
+// @icon        https://cdn3.iconfinder.com/data/icons/watchify-v1-0-80px/80/arrow-down-80px-128.png
 // @require     http://code.jquery.com/jquery.min.js
 // @require     https://openuserjs.org/src/libs/soufianesakhi/node-creation-observer.min.js
+// @include     https://www.facebook.com/
 // @include     https://www.facebook.com/?sk=h_chr
-// @version     1.0.1
+// @version     1.0.3
 // @grant       GM_setValue
 // @grant       GM_getValue
 // ==/UserScript==
@@ -17,6 +23,8 @@ var lastPostButtonAppendSelector = "div._5pcp"
 var blueBarId = "pagelet_bluebar";
 var timestampAttribute = "data-timestamp";
 var loadedStoryByPage = 10;
+var fbUrlPatterns = [new RegExp("https?:\/\/www\.facebook\.com\/\\?sk\=h_chr", "i"),
+    new RegExp("https?:\/\/www\.facebook\.com\/?$", "i")];
 
 var lastPostIconLink = "https://cdn3.iconfinder.com/data/icons/watchify-v1-0-80px/80/arrow-down-80px-128.png";
 var iconStyle = "vertical-align: middle; height: 20px; width: 20px; cursor: pointer;";
@@ -27,7 +35,7 @@ var lastPostSeparatorId = scriptId + "Separator";
 var lastPostURIKey = scriptId + "URI"
 var lastPostTimestampKey = scriptId + "Timestamp"
 
-var lastPostURI = GM_getValue(lastPostURIKey, "");
+var lastPostURI = GM_getValue(lastPostURIKey, null);
 var lastPostTimestamp = GM_getValue(lastPostTimestampKey, 0);
 var storyCount = 0;
 var storyLoadObservers = [];
@@ -35,12 +43,36 @@ var loadedStories = [];
 var checkedStories = [];
 var previousScrollHeight;
 var stopped = false;
+var isMostRecentMode = false;
+var isHome = true;
 
 $(document).ready(function() {
+    checkMostRecentMode();
+    initLastPostButtonObserver();
+    scrollToLastPost();
+});
+
+function checkMostRecentMode() {
+    $("script").each(function() {
+        var text = $(this).text().trim();
+        if (text.startsWith('bigPipe.beforePageletArrive("pagelet_navigation")')) {
+            var textContainingMode = $(this).next().text();
+            var selectedModePattern = /\"selectedKey"\:\"([^\"]*)"/i;
+            var match = textContainingMode.match(selectedModePattern);
+            var modeText = match[1];
+            if (modeText === "h_chr") {
+                isMostRecentMode = true;
+            } else {
+                isMostRecentMode = false;
+            }
+        }
+    });
+}
+
+function initLastPostButtonObserver() {
     NodeCreationObserver.onCreation(lastPostButtonAppendSelector, function(storyDetailsElement) {
-        var fbUrlPattern = new RegExp("https?:\/\/www\.facebook\.com\/\\?sk\=h_chr", "i");
-        var isMostRecentMode = fbUrlPattern.test(document.URL);
-        if (isMostRecentMode) {
+        checkURL();
+        if (isHome && isMostRecentMode) {
             var storyElement = $(storyDetailsElement).closest(storySelector);
             var storyId = storyElement.attr('id');
             var lastPostIconId = getLastPostIconId(storyId);
@@ -53,10 +85,30 @@ $(document).ready(function() {
             });
         }
     });
-    if (lastPostURI === "") {
-        return;
+}
+
+function checkURL() {
+    isHome = matchesFBHomeURL();
+    if (isHome) {
+        checkMostRecentMode();
     }
+}
+
+function matchesFBHomeURL() {
+    var isHome = false;
+    fbUrlPatterns.forEach(function(pattern) {
+        if (pattern.test(document.URL)) {
+            isHome = true;
+        }
+    });
+    return isHome;
+}
+
+function scrollToLastPost() {
     NodeCreationObserver.onCreation(storySelector, function(element) {
+        if (stopped) {
+            return;
+        }
         storyCount++;
         if (loadedStories.indexOf(element) == -1) {
             loadedStories.push(element);
@@ -66,23 +118,27 @@ $(document).ready(function() {
             return;
         }
         if (storyCount == 1) {
-            searchForStory();
             setLastPost(element);
+            if (lastPostURI == null) {
+                NodeCreationObserver.remove(storySelector);
+                stopped = true;
+                return;
+            }
+            searchForStory();
         } else if (storyCount == 2) {
             searchForStory();
-            if (!stopped) {
-                scrollToBottom();
-                storyCount = 10;
-            }
+            scrollToBottom();
+            storyCount = 10;
         }
     });
-});
+}
 
 function setLastPost(storyElement) {
     var uri = getStoryURI(storyElement);
     var timestamp = getStoryTimestamp(storyElement);
     GM_setValue(lastPostURIKey, uri);
     GM_setValue(lastPostTimestampKey, timestamp);
+    console.log("Setting last post: " + uri + "(" + timestamp + ")");
 }
 
 function getLastPostIconId(storyId) {
@@ -122,7 +178,8 @@ function searchForStory() {
                     stopSearching(element.id);
                 } else if (ts < lastPostTimestamp) {
                     stopSearching(element.id);
-                    alert("The last post was not found (https://www.facebook.com" + uri + ")");
+                    console.log("The last post was not found: " + lastPostURI);
+                    console.log("Stopped at the post: " + uri);
                 }
             }
         }
